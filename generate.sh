438833d9725a9f472c05665e9cc94b9d970cef11
becash <git@ci.md>
2025-08-25 16:13:09 +0300

    Clean up `generate.sh` by removing commented-out legacy code and unused commands

--- generate.sh
+++ generate.sh
@@ -51,7 +51,6 @@ for dir in ./gen_go/*/; do
   PROJECT="${dir%/}"           # remove trailing slash
   PROJECT="${PROJECT##*/}"     # keep only the last path component
 
-#  generate documentation, from protofiles
   docker run -v "${PWD}":/root/data protoc-go \
     protoc \
     --proto_path=./proto --doc_out=./proto/"${PROJECT}"/ --doc_opt=markdown,DOCUMENTATION.md ./proto/"${PROJECT}"/*.proto
@@ -60,12 +59,3 @@ for dir in ./gen_go/*/; do
 
   unlink ./proto/"${PROJECT}"/DOCUMENTATION.md
 done
-
-#
-#echo ---------------------GENERATE UML-----------------------------------------
-#docker run -v "${PWD}":/root/data protoc-go ./code_generator/uml/protodot -src ./proto/structure_999/structure.proto -output Category -select Category
-#docker run -v "${PWD}":/root/data protoc-go chmod -R 777 ./gen_uml
-#rm -f ./gen_uml/Category.dot
-#mv -f ./gen_uml/Category.dot.png ./gen_uml/Category.png
-#echo -e "## Actual Category schema\n\n![Actual Category schema](../../gen_uml/Category.png)\n\n$(cat ./proto/structure_999/README.md)" > ./proto/structure_999/README.md
-#
